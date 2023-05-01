f44f9d6305bee228f63c4b81b7ec94c5b7d4c458
Thomas Moore <tom@Thomass-MacBook-Air.local>
2023-05-01 01:38:04 +0100

    fix type errors

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,16 +1,20 @@
-// import { z } from "zod";
+import { z } from "zod";
 
-// import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
-// export const exampleRouter = createTRPCRouter({
-//   hello: publicProcedure
-//     .input(z.object({ text: z.string() }))
-//     .query(({ input }) => {
-//       return {
-//         greeting: `Hello ${input.text}`,
-//       };
-//     }),
-//   getAll: publicProcedure.query(({ ctx }) => {
-//     return ctx.prisma.example.findMany();
-//   }),
-// });
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
