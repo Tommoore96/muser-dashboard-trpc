5abacba817268616c854ac520c335e0210c74b8a
Thomas Moore <tom@Thomass-MacBook-Air.local>
2023-05-01 01:28:02 +0100

    remove example stuff

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+// import { exampleRouter } from "~/server/api/routers/example";
 import { authRouter } from "./routers/auth";
 
 /**
@@ -9,7 +9,7 @@ import { authRouter } from "./routers/auth";
  */
 export const appRouter = createTRPCRouter({
   auth: authRouter,
-  example: exampleRouter,
+  // example: exampleRouter,
 });
 
 // export type definition of API
